20b6a1238d47ed4ff1bd0cb43d57393d393b9a04
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2022-02-11 19:53:10 +0100

    Introduce custom exception to handle missing die attributes and disable function pointers printing

--- dwarf-parser.py
+++ dwarf-parser.py
@@ -18,6 +18,16 @@ class ElfFile(ELFFile):
         atexit.register(lambda: self._fd.close())
 
 
+class NoAttributeInDie(Exception):
+    def __init__(self, die, message=""):
+        self._message = message
+        self._die = die
+        super().__init__(self._message)
+
+    def __str__(self):
+        return f"{self._message}\n{self._die}"
+
+
 def parse(elf_path):
     elf = ElfFile(elf_path)
     if not elf.has_dwarf_info():
@@ -33,65 +43,82 @@ def parse(elf_path):
             f"  Found a compile unit at offset {CU.cu_offset}, length {CU['unit_length']}")
 
         # Start with the top DIE, the root for this CU's DIE tree
-        top_DIE = CU.get_top_DIE()
-        print(f"    Top DIE with tag={top_DIE.tag}")
+        top_die = CU.get_top_DIE()
+        print(f"    Top DIE with tag={top_die.tag}")
 
-        print(f"    name={top_DIE.get_full_path()}")
+        print(f"    name={top_die.get_full_path()}")
 
         # Display DIEs recursively starting with top_DIE
-        die_info_rec(top_DIE)
+        die_info_rec(top_die)
+
+
+def get_attribute_value(die, attribute):
+    try:
+        return die.attributes[attribute].value.decode('utf-8')
+    except KeyError:
+        raise NoAttributeInDie(die)
+
+
+def get_die_from_attribute(die, attribute):
+    try:
+        return die.get_DIE_from_attribute(attribute)
+    except KeyError:
+        raise NoAttributeInDie(die)
 
 
 def get_type(die, name=""):
     try:
-        dw_at_type = die.get_DIE_from_attribute('DW_AT_type')
+        dw_at_type = get_die_from_attribute(die, 'DW_AT_type')
         if dw_at_type.tag == "DW_TAG_const_type":
             name = " const" + name
         if dw_at_type.tag == "DW_TAG_volatile_type":
             name = " volatile" + name
         if dw_at_type.tag == "DW_TAG_pointer_type":
             name = name + "*"
-    except KeyError:
+    except NoAttributeInDie:
         return f"void{name}"
     try:
-        return f"{dw_at_type.attributes['DW_AT_name'].value}{name}"
-    except KeyError:
+        return f"{get_attribute_value(dw_at_type, 'DW_AT_name')}{name}"
+    except NoAttributeInDie:
         try:
             return get_type(dw_at_type, name)
-        except:
-            print("really bad")
+        except Exception:
+            print("Should really not end up here")
 
 
-def inline(die):
+def is_inline(die):
     if "DW_AT_inline" in die.attributes:
         return "inline"
     return ""
 
 
-def process(die):
+def get_name(die):
     try:
-        out = f"{die.attributes['DW_AT_name'].value.decode('utf-8')}"
-    except KeyError:
+        return get_attribute_value(die, 'DW_AT_name')
+    except NoAttributeInDie:
         try:
-            die = die.get_DIE_from_attribute('DW_AT_abstract_origin')
-            out = f"{die.attributes['DW_AT_name'].value.decode('utf-8')}"
-        except KeyError:
-            out = ""  # unnamed parameter, probably in function pointer
-    out = f"{inline(die)} {get_type(die)} {out}"
-    return out
+            die = get_die_from_attribute(die, 'DW_AT_abstract_origin')
+            return get_attribute_value(die, 'DW_AT_name')
+        except NoAttributeInDie:
+            return ""  # unnamed parameter, probably in function pointer
+
+
+def process(die):
+    return f"{is_inline(die)} {get_type(die)} {get_name(die)}"
 
 
 def die_info_rec(die, indent_level='    '):
     """ A recursive function for showing information about a DIE and its
         children.
     """
+    # We are interested only in prototypes, not function pointers
+    if die.tag == "DW_TAG_subroutine_type":
+        return
     if die.tag == "DW_TAG_inlined_subroutine":
         die = die.get_DIE_from_attribute('DW_AT_abstract_origin')
-    # if die.tag == "DW_TAG_entry_point":
-    if die.tag in ["DW_TAG_subprogram", "DW_TAG_formal_parameter",
-                   "DW_TAG_subroutine_type"]:
-        out = process(die)
-        print(f"{indent_level}{out}")
+    # I guess there is no need to support "DW_TAG_entry_point" for now
+    if die.tag in ["DW_TAG_subprogram", "DW_TAG_formal_parameter"]:
+        print(f"{indent_level}{process(die)}")
 
     child_indent = indent_level + '  '
     # print(f"{indent_level} {die.tag}")
